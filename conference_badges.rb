fff131449e515b5db20a17965e25c8ad852804bf
Cody Yantis <cyantis@gmail.com>
2019-01-25 04:15:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,7 @@ def assign_rooms(name)
     room_arr.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
     iterate += 1
   end
-  room_arr.each do |assingment|
-    puts "#{assingment}"
+  room_arr.each do |assignment|
+    puts "#{assignment}"
   end
 end
