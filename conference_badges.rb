046989849d25bef235638790ade800c88f479e8b
Cody Yantis <cyantis@gmail.com>
2019-01-25 03:46:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,9 @@ end
 
 def batch_badge_creator(attendees)
   badge_arr = []
+  iterate = 0
   attendees.each do |name|
     badge_arr.push(badge_maker(name))
-    name +=1
+    iterate += 1
   end
 end
